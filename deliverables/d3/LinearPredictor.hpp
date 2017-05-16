adbf643303c13395adf90d5b4de7c38cb759e958
maikeps <psmaike@gmail.com>
2017-05-16 16:13:53 -0300

    updateCoefficients implemented, not working properly.

--- deliverables/d3/LinearPredictor.hpp
+++ deliverables/d3/LinearPredictor.hpp
@@ -28,7 +28,7 @@ public:
   
   Type predictNext(Type lastValue) {
     Type nextValue = _m * lastValue + _b;
-    
+
     addLastValue(lastValue);    
     updateCoefficients();
     
@@ -48,9 +48,32 @@ protected:
   }
   
   void updateCoefficients() {
-    // TODO Gradient Descent
-  }
-  
+    float b = _b;
+    float m = _m;
+
+    int N = _currentIndex;
+
+    for(int i = 0; i < _gradientIterations; i++) {
+      float b_gradient = 0;
+      float m_gradient = 0;
+     
+      for(int j = 0; j < _currentIndex; j++) {
+        int x = j+1;
+        float y = _dataWindow[j];
+
+        b_gradient += -(2.0/N) * (y - ((m * x) + b));
+        m_gradient += -(2.0/N) * x * (y - ((m * x) + b));
+      }
+
+      b = b - (_learningRate * b_gradient);
+      m = m - (_learningRate * m_gradient);
+    }
+
+    _b = b;
+    _m = m;
+
+    std::cout << "New function:\ny = " << _m << "x + " << _b << std::endl;
+  }  
 };
 
 #endif // LINEAR_PREDICTOR_HPP_
