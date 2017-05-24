2de0c566c443a53a43b38ca5e2a504b752e9842d
maikeps <psmaike@gmail.com>
2017-05-23 22:23:06 -0300

    Plot

--- deliverables/d3/plotStats1.r
+++ deliverables/d3/plotStats1.r
@@ -1,24 +1,24 @@
 ##############################
 #       Test Stats Plot      #
 ##############################
-data <- read.csv("data/data1_results.txt", header=TRUE)
+data <- read.csv("data/data2_results.txt", header=TRUE)
 
-png("data/data1_plot.png", width=800, height=500)
+png("data/data2_plot.png", width=800, height=500)
 
 colorReal <- "darkgreen"
 colorPredicted <- "orange"
 colorDiff <- "red"
 
-numberPoints <- 3000 #length(data[, 1])
+numberPoints <- length(data[, 1])
 realValues <- head(data[, 1], n=numberPoints)
 predictedValues <- head(data[, 2], n=numberPoints)
 diffValues <- head(data[, 3], n=numberPoints)
 
-plot(1:numberPoints, realValues, type="l", col=colorReal, xlab="Time (t)", ylab="Temperature (ºC)")
-title(main="Prediction of Hourly Air Temperature at the San Francisco International Airport\nfrom January 1st, 2015 to December 31st, 2016")
+plot(1:numberPoints, realValues, type="l", col=colorReal, xlab="Time (x)", ylab="y = 3x + 7")
+title(main="Prediction of Function y = 3x + 7")
 lines(1:numberPoints, predictedValues, type="l", col=colorPredicted)
 lines(1:numberPoints, diffValues, type="p", col=colorDiff)
 legend("topleft",
-       legend=c("Real Temperature", "Predicted Temperature", "| Real - Predicted |"),
+       legend=c("RealY", "PredictedY", "| RealY - PredictedY |"),
        lty=c(1, 1, 0), pch=c(NA, NA, 1), col=c(colorReal, colorPredicted, colorDiff))
 
