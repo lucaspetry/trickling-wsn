0e8088e6769aee32d2ac177e1129f4861da8f7e7
Lucas May Petry <lucasmaypetry@gmail.com>
2017-06-25 13:58:05 -0300

    Fix MLP

--- deliverables/d3/main_mlp.cpp
+++ deliverables/d3/main_mlp.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 #include "mlp_predictor.hpp"
 
-#define HIDDEN_UNITS 10
+#define HIDDEN_UNITS 5
+#define INPUTS 3
 
-using array = std::array<float, HIDDEN_UNITS>;
+using arrayInput = std::array<double, INPUTS>;
+using array = std::array<double, HIDDEN_UNITS>;
+using matrix = std::array<arrayInput, HIDDEN_UNITS>;
 
 int main(int argc, char* argv[]) {
-	array wInput = {-0.8757484831774061, 0.638223177490687, 0.47439169507765233, -0.6241506181598983, 0.5809774819295261, -0.7266780203467283, -1.147251210290187, 0.25788441254322364};
-	array biasInput = {-0.3414027283264503, -0.39535832398743515, -0.3901928811585848, -0.42176012510025584, -0.38254522241975947, -0.367525877577434, -0.2181034777366183, -0.4042664364294075};
-	array wHiddenLayer = {-0.8152340897831858, 0.5940886042485971, 0.4444262681600428, -0.5257855337232896, 0.5410347272911142, -0.6329135540816879, -1.3565282423001854, 0.2553571353168649};
-	float biasHidden = 0.6710505765521378;
+	matrix wInput;
+	wInput[0] = {-0.6183, -0.7241, 4.6622};
+	wInput[1] = {-2.2563, 4.1614, 0.9684};
+	wInput[2] = {-1.1493, 1.4208, 4.3769};
+	wInput[3] = {-2.2230, -0.8643, -4.0633};
+	wInput[4] = {3.0342, 3.2611, -1.5842};
+	array biasInput = {4.8398, 2.2577, -0.2679, -2.3915, 4.8339};
+	array wHiddenLayer = {-0.4124, 0.8962, 0.6646, -0.1890, -0.5212};
+	double biasHidden = -0.1443;
 
-	float minValue = 3.3;
-	float maxValue = 33.3;
+	double minValue = 1.7;
+	double maxValue = 33.9;
 
- 	auto predictor = new MLPPredictor<float, HIDDEN_UNITS>(wInput, biasInput, wHiddenLayer, biasHidden, true, minValue, maxValue);
+ 	auto predictor = new MLPPredictor<double, INPUTS, HIDDEN_UNITS>(wInput, biasInput, wHiddenLayer,
+ 																	biasHidden, false, minValue, maxValue);
 
- 	float lastValue = 16.1;
- 	for(int i = 0; i < 400; i++) {
- 		lastValue = predictor->predictNext(lastValue);
- 		std::cout << lastValue << std::endl;	
+ 	double inputs[] = {0.2578, 0.1708, 0.1708, 0.1708, 0.1366, 0.1553, 0.0839, 0.2578, 0.2391, 0.2391, 0.2391};
+ 	double expect[] = {0.2574, 0.2227, 0.1487, 0.2038, 0.1930, 0.1728, 0.1917, 0.1962, 0.3352, 0.2335, 0.2433};
+
+	std::cout << "Matlab\t\tOutput\n" << std::endl;
+ 	for(int i = 0; i < 11; i++) {
+ 		double lastValue = predictor->predictNext(inputs[i]);
+ 		std::cout << expect[i] << "\t\t" << lastValue << std::endl;
  	}
 
   	return 0;
--- deliverables/d3/mlp_predictor.hpp
+++ deliverables/d3/mlp_predictor.hpp
@@ -5,23 +5,27 @@
 #include <array>
 #include <math.h>
 
-
-template<class Type, unsigned short HIDDEN_UNITS>
+template<class Type, unsigned short INPUTS, unsigned short HIDDEN_UNITS>
 class MLPPredictor : public Predictor<Type> {
 
-using array = std::array<float, HIDDEN_UNITS>;
+using arrayInput = std::array<double, INPUTS>;
+using array = std::array<double, HIDDEN_UNITS>;
+using matrix = std::array<arrayInput, HIDDEN_UNITS>;
 
 private:
-  array _wInput; // pesos saindo do nodo de entrada para a hidden layer
+  arrayInput _lastInputs;
+  matrix _wInput; // pesos saindo do nodo de entrada para a hidden layer
   array _biasInput; // bias entrando na hidden layer, junto com o nodo de entrada
   array _wHiddenLayer; // pesos saindo de cada item da hidden layer para o nodo de saida
-  float _biasHidden; // bias entrando na output layer, junto com a hidden layer
+  double _biasHidden; // bias entrando na output layer, junto com a hidden layer
 
   bool _isNormalized;
   Type _trainingMin;
   Type _trainingMax;
+  unsigned int _currentIndex;
+
 public:
-  MLPPredictor(array wInput, array biasInput, array wHiddenLayer, float biasHidden, bool isNormalized = false, Type trainingMin = 0, Type trainingMax = 0) {
+  MLPPredictor(matrix wInput, array biasInput, array wHiddenLayer, double biasHidden, bool isNormalized = false, Type trainingMin = 0, Type trainingMax = 0) {
         _wInput = wInput;
         _biasInput = biasInput;
         _wHiddenLayer = wHiddenLayer;
@@ -30,9 +34,25 @@ public:
         _isNormalized = isNormalized;
         _trainingMin = trainingMin;
         _trainingMax = trainingMax;
+
+        _currentIndex = 0;
+
+        for(int i = 0; i < INPUTS; i++)
+          _lastInputs[i] = 0.2578;
     }
 
   ~MLPPredictor() { }
+
+  void addLastValue(Type lastValue) {
+    if(_currentIndex >= INPUTS) {
+      for(unsigned int i = 1; i < INPUTS; i++)
+        _lastInputs[i - 1] = _lastInputs[i];
+      
+      _currentIndex = INPUTS - 1;
+    }
+   
+    _lastInputs[_currentIndex++] = lastValue;
+  }
 
   inline Type normalize(Type nonNormalizedValue) {
     return (nonNormalizedValue - _trainingMin) / (_trainingMax - _trainingMin);
@@ -43,40 +63,39 @@ public:
   }
 
   Type predictNext(Type lastValue) {
-    if(_isNormalized) {
-      lastValue = normalize(lastValue);
-    }
+    if(_isNormalized) lastValue = normalize(lastValue);
+    addLastValue(lastValue);
 
   	// multiplica a entrada pelos pesos de entrada e soma com o bias
   	// para cada um dos N neuronios intermediarios
   	// e passa pela função de ativação
-    float outputHiddenLayer[HIDDEN_UNITS];
-    float summation = 0;
+    double outputHiddenLayer[HIDDEN_UNITS];
     for(int i = 0; i < HIDDEN_UNITS; i++) {
-   		summation = lastValue * _wInput[i] + _biasInput[i];
-     	outputHiddenLayer[i] = activationFunction(summation);
+      double sum = 0;
+      for(int j = 0; j < INPUTS; j++) {
+          sum += _lastInputs[j] * _wInput[i][j];
+      }
+
+   		sum += _biasInput[i];
+     	outputHiddenLayer[i] = activationFunction(sum);
     }
 
     //multiplica e soma os valores no nodo de saida
-    float sumOutputLayer = _biasHidden;
+    double sumOutputLayer = _biasHidden;
     for(int i = 0; i < HIDDEN_UNITS; i++) {
       sumOutputLayer += outputHiddenLayer[i] * _wHiddenLayer[i];
     }
 
     Type output = outputActivationFunction(sumOutputLayer);
-
-    if(_isNormalized) {
-      output = denormalize(output);
-    }
-
+    if(_isNormalized) output = denormalize(output);
     return output;
   }
 
-  float activationFunction(float summation) {
+  double activationFunction(double summation) {
     return 1/(1+exp(-summation));
   }
 
-  inline float outputActivationFunction(float summation) {
+  inline double outputActivationFunction(double summation) {
     return summation;
   }
 };
